1ceb216521e216c45e2d4f82dd2510268331d80d
a.cicconi <analuiza.cicconi@blablacar.com>
2020-07-10 17:43:45 +0200

    in progress

--- app/src/main/java/com/cicconi/events/EventDetailsActivity.java
+++ app/src/main/java/com/cicconi/events/EventDetailsActivity.java
@@ -1,9 +1,5 @@
 package com.cicconi.events;
 
-import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,35 +10,5 @@ public class EventDetailsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_events_all) {
-            Intent mainActivityIntent = new Intent(this, MainActivity.class);
-            startActivity(mainActivityIntent);
-
-            return true;
-        }
-
-        if (id == R.id.action_events_favorite) {
-            Intent mainActivityIntent = new Intent(this, MainActivity.class);
-            mainActivityIntent.putExtra(Constants.EXTRA_CATEGORY_TYPE, CategoryType.FAVORITE);
-            startActivity(mainActivityIntent);
-
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }

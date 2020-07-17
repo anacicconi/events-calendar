31cc08d05e862816bcd4dc166b735fb5af852c23
a.cicconi <analuiza.cicconi@blablacar.com>
2020-07-17 20:18:50 +0200

    release

--- app/src/androidTest/java/com/cicconi/events/EventDetailsActivityTest.java
+++ app/src/androidTest/java/com/cicconi/events/EventDetailsActivityTest.java
@@ -6,18 +6,14 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import com.cicconi.events.database.Event;
-import com.google.android.exoplayer2.SimpleExoPlayer;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class) public class EventDetailsActivityTest {
 
